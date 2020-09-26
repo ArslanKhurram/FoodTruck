de3c0de8937885dfcf22e37e038d03eace8c12ec
ArslanKhurram <marslankhurram@gmail.com>
2020-09-26 17:34:01 -0400

    vendor table is complete and splash screen is made

--- app/src/main/java/com/example/foodtruck/DataBase/DbHelper.java
+++ app/src/main/java/com/example/foodtruck/DataBase/DbHelper.java
@@ -8,6 +8,7 @@ import com.example.foodtruck.models.Customer;
 
 import static com.example.foodtruck.DataBase.PaymentsContract.PaymentsEntry;
 import static com.example.foodtruck.DataBase.CustomersContract.CustomersEntry;
+import static com.example.foodtruck.DataBase.VendorsContract.VendorsEntry;
 
 
 public class DbHelper extends SQLiteOpenHelper {
@@ -44,16 +45,34 @@ public class DbHelper extends SQLiteOpenHelper {
             CustomersEntry.COL_STATE + " TEXT NOT NULL" +
             "); ";
 
+    //query to create  customer table
+    public static final String SQL_CREATE_VENDORS_TABLE = "CREATE TABLE " + VendorsEntry.TABLE_NAME + " (" +
+            VendorsEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+            VendorsEntry.COL_FIRST_NAME + " TEXT NOT NULL, " +
+            VendorsEntry.COL_LAST_NAME + " TEXT NOT NULL, " +
+            VendorsEntry.COL_EMAIL + " TEXT NOT NULL, " +
+            VendorsEntry.COL_PHONE_NUMBER + " TEXT NOT NULL, " +
+            VendorsEntry.COL_STREET_NAME + " TEXT NOT NULL, " +
+            VendorsEntry.COL_HOUSE_NUMBER + " TEXT NOT NULL, " +
+            VendorsEntry.COL_ZIP_CODE + " TEXT NOT NULL, " +
+            VendorsEntry.COL_CITY + " TEXT NOT NULL, " +
+            VendorsEntry.COL_STATE + " TEXT NOT NULL, " +
+            VendorsEntry.COL_DATE_ADDED + " TEXT NOT NULL, " +
+            VendorsEntry.COL_CATEGORY + " TEXT NOT NULL" +
+            "); ";
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(SQL_CREATE_PAYMENTS_TABLE);
         db.execSQL(SQL_CREATE_CUSTOMERS_TABLE);
+        db.execSQL(SQL_CREATE_VENDORS_TABLE);
+
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS " + PaymentsEntry.TABLE_NAME);
-        db.execSQL("DROP TABLE IF EXISTS " + CustomersContract.CustomersEntry.TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS " + CustomersEntry.TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS " + VendorsEntry.TABLE_NAME);
 
     }
 }
