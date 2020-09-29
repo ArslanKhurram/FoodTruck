6c573598c49c8fd0700fe7f7d64f0fa838c07146
Tnofi <TNofiFSC@users.noreply.github.com>
2020-09-29 19:49:49 -0400

    Prototype login & signup to test the manipulation of user table db values

--- app/src/main/java/com/example/foodtruck/MainActivity.java
+++ app/src/main/java/com/example/foodtruck/MainActivity.java
@@ -2,16 +2,13 @@ package com.example.foodtruck;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.net.LocalSocketAddress;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
 import com.example.foodtruck.DataBase.PaymentsContract;
 import com.example.foodtruck.DataBase.CustomersContract;
-import com.example.foodtruck.DataBase.VendorsContract;
 import com.example.foodtruck.models.Customer;
-
-import java.util.Calendar;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -25,20 +22,20 @@ public class MainActivity extends AppCompatActivity {
     public void makeEntry(View view) {
 
         CustomersContract cc = new CustomersContract(this);
+        cc.open();
         cc.addCustomer("Arslan", "Adam", "khura6@farmingdale.edu","631-123-4567", "Apple St","1234","11777","Adams Town","PA");
-
-
-
-        VendorsContract vc = new VendorsContract(this);
-        vc.addVendor("Mike","Pear","mpwar@gmail.com","516-920-0202","Maple Ave","987","10393","Apple Village","VA", (Calendar.getInstance().getTime()).toString(), "Pizza")  ;
-
 
         Customer cus;
         cus = cc.getCustomerIdByEmail("khura6@farmingdale.edu");
 
-
         PaymentsContract pc = new PaymentsContract(this);
+        pc.open();
         pc.createPayment("Credit Card", "John Doe", "05/22", "123", "09/25/20", cus.getM_Id());
 
+    }
+
+    public void ToSignUp(View view) {
+        Intent i = new Intent(MainActivity.this, activity_signup.class);
+        startActivity(i);
     }
 }
