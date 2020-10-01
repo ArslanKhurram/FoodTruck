1df88e19bfdc43877da05a9065787d135c66b991
Bryan <gonzb3@farmingdale.edu>
2020-10-01 03:21:04 -0400

    created fragments for login and signup

--- app/src/main/java/com/example/foodtruck/activity_signup.java
+++ app/src/main/java/com/example/foodtruck/activity_signup.java
@@ -1,9 +1,13 @@
 package com.example.foodtruck;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.Spinner;
 
@@ -13,25 +17,19 @@ public class activity_signup extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.frag_signup);
+        setContentView(R.layout.activity_signup);
+
+        //open login fragment on start
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.test,new LoginFragment()).commit();
+        }
+
+
+
     }
 
-    public void CreateAccount(View v){
-        EditText txtFName = findViewById(R.id.etFirstName);
-        EditText txtLname = findViewById(R.id.etLastName);
-        EditText txtEmail = findViewById(R.id.etEmailAdd);
-        EditText txtPhone = findViewById(R.id.etPhone);
-        EditText txtStreet = findViewById(R.id.etStreetAdd);
-        EditText txtCity = findViewById(R.id.etCity);
-        EditText txtZip = findViewById(R.id.etZip);
-        EditText txtPassword = findViewById(R.id.etPassword);
-        Spinner txtState = findViewById(R.id.spnState);
 
-        // New customer entry that will be filled in with the values of of the text boxes
-        CustomersContract cc = new CustomersContract(this);
-        cc.open();
-        cc.addCustomer(txtFName.getText().toString(), txtLname.getText().toString(), txtEmail.getText().toString(),txtPassword.getText().toString() ,txtPhone.getText().toString(), txtStreet.getText().toString(),"123", txtZip.getText().toString(), txtCity.getText().toString(), txtState.getSelectedItem().toString());
-        cc.close();
-        finish();
-    }
+
+
+
 }
