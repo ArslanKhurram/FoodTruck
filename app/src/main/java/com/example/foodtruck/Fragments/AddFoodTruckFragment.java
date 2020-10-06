f68dd72bcab73ea841966a644bc9a9e5050175fa
Jordan <codyja@farmingdale.edu>
2020-10-05 20:11:15 -0400

    Start of AddFoodTruckFragment - adding picture to foodtruck

--- app/src/main/java/com/example/foodtruck/Fragments/AddFoodTruckFragment.java
+++ app/src/main/java/com/example/foodtruck/Fragments/AddFoodTruckFragment.java
@@ -9,6 +9,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -16,20 +19,46 @@ import androidx.fragment.app.Fragment;
 
 import com.example.foodtruck.R;
 
+import static android.app.Activity.RESULT_OK;
+
 public class AddFoodTruckFragment extends Fragment {
 
+    ImageView picView;
+    Button picBttn;
+    EditText etName;
+    EditText etCategory;
+    Spinner spFoodType;
+    Button back;
+    Button submit;
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         View v = inflater.inflate(R.layout.fragment_add_food_truck, container, false);
 
-        Button picBttn = v.findViewById(R.id.);
+        picView = v.findViewById(R.id.picView);
+        picBttn = v.findViewById(R.id.spnImageUpload);
+        etName = v.findViewById(R.id.etName);
+        etCategory = v.findViewById(R.id.etCategory);
+        spFoodType = v.findViewById(R.id.spnPaymentType);
+        back = v.findViewById(R.id.btnBack);
+        submit = v.findViewById(R.id.btnSubmit);
 
+        //choose an image from gallery after clicking button
         picBttn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-
+                Intent pickPhoto = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(pickPhoto, 1);
             }
         });
-
         return v;
+    }
+
+    //result sets image to an image view
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK && data != null) {
+            Bitmap selectedImage = (Bitmap) data.getExtras().get("data");
+            picView.setImageBitmap(selectedImage);
+        }
+
     }
 }
