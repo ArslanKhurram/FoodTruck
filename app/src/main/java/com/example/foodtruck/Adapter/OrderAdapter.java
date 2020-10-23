4ecc77f50d38a7556cb3a2d37d1f3859691bb5e9
Jordan <codyja@farmingdale.edu>
2020-10-23 17:07:47 -0400

    Don't merge to main - still working on orders

---
+++ app/src/main/java/com/example/foodtruck/Adapter/OrderAdapter.java
@@ -0,0 +1,91 @@
+package com.example.foodtruck.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.DiffUtil;
+import androidx.recyclerview.widget.ListAdapter;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.foodtruck.Models.Order;
+import com.example.foodtruck.R;
+
+import java.util.List;
+
+public class OrderAdapter extends ListAdapter<Order, OrderAdapter.OrderViewHolder> {
+    private Context orderContext;
+    private OnOrderListener mOnOrderListener;
+
+    public OrderAdapter(Context context, OnOrderListener onOrderListener) {
+        super(DIFF_CALLBACK);
+        orderContext = context;
+        mOnOrderListener = onOrderListener;
+    }
+
+    //comparison method to add animations to recycler view
+    private static DiffUtil.ItemCallback<Order> DIFF_CALLBACK = new DiffUtil.ItemCallback<Order>() {
+        @Override
+        public boolean areItemsTheSame(@NonNull Order oldOrder, @NonNull Order newOrder) {
+            return oldOrder.getM_Id() == newOrder.getM_Id();
+        }
+
+        @Override
+        public boolean areContentsTheSame(@NonNull Order oldOrder, @NonNull Order newOrder) {
+            return oldOrder.getM_OrderNumber().equals(newOrder.getM_OrderNumber());
+
+        }
+    };
+
+    @NonNull
+    @Override
+    public OrderViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        //inflate list item
+        View OrderView = LayoutInflater.from(parent.getContext()).inflate(R.layout.orders_card, parent, false);
+
+        return new OrderViewHolder(OrderView, mOnOrderListener);
+    }
+
+    //Bind data to position
+    @Override
+    public void onBindViewHolder(OrderViewHolder holder, int position) {
+        holder.BindData(getOrder(position), orderContext);
+    }
+
+    public Order getOrder(int position) {
+        return getOrder(position);
+    }
+
+
+    //viewHolder class whose objects represent each list items
+    public static class OrderViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        private TextView orderNum, orderName, orderQuantity;
+        private OnOrderListener onOrderListener;
+
+        public OrderViewHolder(View view, OnOrderListener onOrderListener) {
+            super(view);
+            orderNum.findViewById(R.id.tvOrderNum);
+            orderName.findViewById(R.id.tvName);
+            orderQuantity.findViewById(R.id.tvQuantity);
+            this.onOrderListener = onOrderListener;
+
+            view.setOnClickListener(this);
+        }
+
+        public void BindData(Order order, Context context) {
+            orderNum.setText(order.getM_OrderNumber());
+        }
+
+        @Override
+        public void onClick(View v) {
+            onOrderListener.onOrderClick(getAdapterPosition());
+        }
+    }
+
+        public interface OnOrderListener {
+            void onOrderClick(int position);
+        }
+}
--- app/src/main/java/com/example/foodtruck/Fragments/Vendor/OrderFragment.java
+++ app/src/main/java/com/example/foodtruck/Fragments/Vendor/OrderFragment.java
@@ -1,28 +1,101 @@
 package com.example.foodtruck.Fragments.Vendor;
 
+import android.app.AlertDialog;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.foodtruck.Adapter.MenuAdapter;
+import com.example.foodtruck.Adapter.OrderAdapter;
+import com.example.foodtruck.DataBase.FoodTrucksContract;
+import com.example.foodtruck.DataBase.ItemsContract;
+import com.example.foodtruck.DataBase.MenusContract;
+import com.example.foodtruck.DataBase.OrdersContract;
+import com.example.foodtruck.DataBase.VendorsContract;
+import com.example.foodtruck.Models.Item;
+import com.example.foodtruck.Models.Order;
+import com.example.foodtruck.Models.Vendor;
 import com.example.foodtruck.R;
 
-public class OrderFragment extends Fragment implements View.OnClickListener {
+import java.util.ArrayList;
 
-    @Nullable
-    @Override
+public class OrderFragment extends Fragment implements OrderAdapter.OnOrderListener, View.OnClickListener {
+    private RecyclerView pendingRecyclerView;
+    private RecyclerView completedRecyclerView;
+    private RecyclerView.Adapter recyclerAdapter;
+    private RecyclerView.Adapter recyclerAdapter2;
+    private OrderAdapter orderAdapter;
+    private OrderAdapter orderAdapter2;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private RecyclerView.LayoutManager mLayoutManager2;
+    private SharedPreferences sharedPref;
+    private Order order;
+    private ArrayList<Order> orderList = new ArrayList<>();
+
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_orders, container, false);
+
+
+        pendingRecyclerView = v.findViewById(R.id.pendingRecycler);
+        pendingRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(getContext()); //use linear layout on cards
+        pendingRecyclerView.setLayoutManager(mLayoutManager);
+        orderAdapter = new OrderAdapter(getContext(), this);
+        orderAdapter.submitList(orderList);
+        recyclerAdapter = orderAdapter;//specify adapter and pass in item list
+        pendingRecyclerView.setAdapter(recyclerAdapter);
+
+        completedRecyclerView = v.findViewById(R.id.completedRecycler);
+        completedRecyclerView.setHasFixedSize(true);
+        mLayoutManager2 = new LinearLayoutManager(getContext());
+        completedRecyclerView.setLayoutManager(mLayoutManager2);
+        orderAdapter2 = new OrderAdapter(getContext(), this);
+        recyclerAdapter2 = orderAdapter2;
+        completedRecyclerView.setAdapter(recyclerAdapter2);
+
 
         return v;
     }
 
+    //get order list from order database
+    public ArrayList<Order> getOrderList() {
+        String email = sharedPref.getString("Email", "");
+
+        VendorsContract vc = new VendorsContract(getContext());
+        OrdersContract oc = new OrdersContract(getContext());
+
+        Vendor vendor = vc.getVendorIdByEmail(email);
+        orderList = oc.getOrdersList(vendor.getM_Id());
+        return orderList;
+    }
+
     @Override
     public void onClick(View v) {
-
+        //addOrderDialog();
     }
+
+    @Override
+    public void onOrderClick(int position) {
+     orderAdapter.getOrder(position);
+    }
+/*
+    private void addOrderDialog()
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        builder.setTitle("OrderNum");
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+ */
 }
